85a1dda3f2435d033eb77b64f74b02db597938ce
Yuri Bobrov <yuri@bobrof.info>
2020-05-20 15:14:24 +0300

    KrakenExcel project added

---
+++ KrakenExcel/Functions.cs
@@ -0,0 +1,54 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.Registration;
+using KrakenClientConsole;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KrakenExcel
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            ExcelRegistration.GetExcelFunctions()
+                           .ProcessAsyncRegistrations(nativeAsyncIfAvailable: false)
+                           .RegisterFunctions();
+        }
+        public void AutoClose()
+        {
+        }
+
+    }
+    public static class Functions
+    {
+        [ExcelFunction(Name = "Kraken.GetBalance", Description = "Kraken API get asset balance")]
+        public async static Task<decimal> GetBalance(string name)
+        {
+            return await Helper.GetBalance(name);
+        }
+        [ExcelFunction(Name = "Kraken.GetTradeBalance", Description = "Kraken API get trade balance")]
+        public async static Task<decimal> GetTradeBalance(string name)
+        {
+            return await Helper.GetTradeBalance(name, "td");
+        }
+        [ExcelFunction(Name = "Kraken.GetEquityBalance", Description = "Kraken API get equity balance")]
+        public async static Task<decimal> GetEquityBalance(string name)
+        {
+            return await Helper.GetTradeBalance(name, "eb");
+        }
+
+        [ExcelFunction(Name = "Kraken.GetTickerAsk", Description = "Kraken API get ticker ask price", ExplicitRegistration = true)]
+        public static async Task<decimal> GetTickerAsk([ExcelArgument(Name = "Asset")] string name)
+        {
+            return await Helper.GetTicker(name.ToString(), "a");
+        }
+        [ExcelFunction(Name = "Kraken.GetTickerBid", Description = "Kraken API get ticker bid price")]
+        public static async Task<decimal> GetTickerBid(string name)
+        {
+            return await Helper.GetTicker(name, "b");
+        }
+
+
+    }
+}
---
+++ KrakenExcel/Helper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using KrakenClient;
+using KrakenClientConsole;
+using NLog;
+
+namespace KrakenExcel
+{
+    public class Helper
+    {
+        private static NLog.Logger nLogger = LogManager.GetCurrentClassLogger();
+        private static KrakenClient.KrakenClient client = new KrakenClient.KrakenClient();
+        private static Broker broker = new Broker();
+
+        #region For Excel
+        public async static Task<decimal> GetBalance(string asset)
+        {
+            decimal balanceAsset = 0;
+            await Task.Run(() =>
+            {
+                var balance = client.GetBalance();
+                var jsSerializer = new JavaScriptSerializer();
+                var result = jsSerializer.DeserializeObject(balance["result"].ToString());
+                var objBalances = (Dictionary<string, object>)result;
+                balanceAsset = Convert.ToDecimal(objBalances[asset]);
+            });
+            return balanceAsset;
+        }
+        public async static Task<decimal> GetTradeBalance(string asset, string value)
+        {
+            decimal balanceAsset = 0;
+            await Task.Run(() =>
+            {
+                var balance = client.GetTradeBalance(null, asset);
+                var jsSerializer = new JavaScriptSerializer();
+                var result = jsSerializer.DeserializeObject(balance["result"].ToString());
+                var objBalances = (Dictionary<string, object>)result;
+                balanceAsset = Convert.ToDecimal(objBalances[value]);
+            });
+            return balanceAsset;
+        }
+        public async static Task<decimal> GetTicker(string pair, string value)
+        {
+            decimal price = 0;
+            await Task.Run(() =>
+            {
+                var ticker = client.GetTicker(new List<string> { pair });
+                var jsSerializer = new JavaScriptSerializer();
+                var result = jsSerializer.DeserializeObject(ticker["result"].ToString());
+                var objPrices = (Dictionary<string, object>)result;
+                var objPrice = (Dictionary<string, object>)objPrices[pair];
+                price = Convert.ToDecimal(((object[])objPrice[value])[0].ToString());
+            });
+            return price;
+        }
+        #endregion
+    }
+}
